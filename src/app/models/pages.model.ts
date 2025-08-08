0bdd6c4edd02b51f5e7f651822131873d97c7e1f
Nicholas (Spefire) <spefire@gmail.com>
2025-08-08 12:11:02 +0200

    Remove Spefire on titles

--- src/app/models/pages.model.ts
+++ src/app/models/pages.model.ts
@@ -1,5 +1,5 @@
 export class PageTitles {
-  public static app = 'Nicholas/Spefire | Partenaire de Vie';
+  public static app = 'Nicholas | Partenaire de Vie';
   public static contact = `Contact`;
   public static legacy = `Mentions légales`;
   public static welcome = `Accueil`;
