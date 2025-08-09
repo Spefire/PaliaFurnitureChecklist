86b49a6b1952231ea7472e1f6c0e293e02dd7fc7
Nicholas (Spefire) <spefire@gmail.com>
2025-08-09 17:19:42 +0200

    Add personnality stats and prepare fixes

--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -4,12 +4,13 @@ import { MainGalleryComponent } from '@src/pages/main/main-gallery/main-gallery.
 import { MainHeaderComponent } from '@src/pages/main/main-header/main-header.component';
 import { MainHobbiesComponent } from '@src/pages/main/main-hobbies/main-hobbies.component';
 import { MainProjectComponent } from '@src/pages/main/main-project/main-project.component';
+import { MainStatsComponent } from '@src/pages/main/main-stats/main-stats.component';
 import { SharedModule } from '@src/shared.module';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [SharedModule, MainHeaderComponent, MainGalleryComponent, MainHobbiesComponent, MainProjectComponent],
+  imports: [SharedModule, MainHeaderComponent, MainGalleryComponent, MainHobbiesComponent, MainProjectComponent, MainStatsComponent],
   templateUrl: './main.page.html',
   styleUrls: ['./main.page.scss'],
 })
