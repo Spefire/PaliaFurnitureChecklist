10f7d1506de99cebc002d673dfba155cbf6337d6
Nicholas (Spefire) <spefire@gmail.com>
2025-08-13 19:55:48 +0200

    Clean images

--- src/app/pages/main/main-gallery/main-gallery.component.ts
+++ src/app/pages/main/main-gallery/main-gallery.component.ts
@@ -14,37 +14,37 @@ import { SharedModule } from '@src/shared.module';
 export class MainGalleryComponent {
   public photos: Photo[] = [
     {
-      src: 'assets/gallery/me-0.png',
+      src: 'assets/gallery/me-0.jpg',
       alt: 'Photo de moi n°1',
       description: `Découverte d'une passion des bretelles`,
       category: 'Vie quotidienne',
     },
     {
-      src: 'assets/gallery/me-1.png',
+      src: 'assets/gallery/me-1.jpg',
       alt: 'Photo de moi n°2',
       description: 'Paré pour une randonnée en montagne',
       category: 'Voyage',
     },
     {
-      src: 'assets/gallery/me-2.png',
+      src: 'assets/gallery/me-2.jpg',
       alt: 'Photo de moi n°3',
       description: 'Sieste du weekend...',
       category: 'Vie quotidienne',
     },
     {
-      src: 'assets/gallery/me-3.png',
+      src: 'assets/gallery/me-3.jpg',
       alt: 'Photo de moi n°4',
       description: 'Rapide séjour à Paris donc "photo Eiffel" obligée !',
       category: 'Voyage',
     },
     {
-      src: 'assets/gallery/me-4.png',
+      src: 'assets/gallery/me-4.jpg',
       alt: 'Photo de moi n°5',
       description: 'Avant la séance, encore frais...',
       category: 'Sport',
     },
     {
-      src: 'assets/gallery/me-5.png',
+      src: 'assets/gallery/me-5.jpg',
       alt: 'Photo de moi n°6',
       description: 'Moi qui te juge.',
       category: 'Vie quotidienne',
