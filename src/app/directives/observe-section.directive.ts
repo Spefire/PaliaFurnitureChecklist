c4c7199d3bff4b818162caba174dee960a55bfe5
Nicholas (Spefire) <spefire@gmail.com>
2025-08-13 19:36:00 +0200

    Fix scroll

--- src/app/directives/observe-section.directive.ts
+++ src/app/directives/observe-section.directive.ts
@@ -1,25 +1,21 @@
+import { Location } from '@angular/common';
 import { AfterViewInit, Directive, ElementRef, inject, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Directive({
   selector: 'section[appObserveSection]',
 })
 export class ObserveSectionDirective implements AfterViewInit, OnDestroy {
   private _el = inject(ElementRef<HTMLElement>).nativeElement;
-  private _router = inject(Router);
+  private _location = inject(Location);
   private _observer?: IntersectionObserver;
 
   public ngAfterViewInit() {
     this._observer = new IntersectionObserver(
       entries => {
         const e = entries[0];
-        if (e.isIntersecting && this._el.id) {
-          this._router.navigate([], {
-            fragment: this._el.id,
-            replaceUrl: true,
-            queryParamsHandling: 'merge',
-          });
-        }
+        if (!e.isIntersecting || !this._el.id) return;
+        const path = this._location.path().split('#')[0];
+        this._location.replaceState(path + '#' + this._el.id);
       },
       {
         root: null,
