09ba4c43d3ae7a9d1f7d4afe69985fd75e811f8e
Nicholas (Spefire) <spefire@gmail.com>
2025-08-13 18:47:35 +0200

    Add hot section

--- src/app/pages/main/main-hot/main-hot.component.ts
+++ src/app/pages/main/main-hot/main-hot.component.ts
@@ -1,58 +1,92 @@
 import { Component } from '@angular/core';
 
-import { NoteComponent } from '@src/components/note/note.component';
 import { ObserveSectionDirective } from '@src/directives/observe-section.directive';
 import { SharedModule } from '@src/shared.module';
 
 @Component({
   selector: 'main-hot',
   standalone: true,
-  imports: [SharedModule, NoteComponent, ObserveSectionDirective],
+  imports: [SharedModule, ObserveSectionDirective],
   templateUrl: './main-hot.component.html',
   styleUrls: ['./main-hot.component.scss'],
 })
 export class MainHotComponent {
   public isRevealed = false;
+  public isRevealedExtra = false;
+  public isRevealedVeryExtra = false;
+  public showPhoto = false;
 
   public hotTopics = [
     {
+      icon: '↕️​',
+      title: 'Versatile',
+      description: `Parfois passif, souvent actif, dominant ou dominé, j'aime varier selon l'envie, le moment et la personne...
+      Et pas forcément de la pénétration : on peut se faire plaisir un tas de manière 😜​`,
+    },
+    {
       icon: '💕',
-      title: 'Intimité physique',
-      description: `J'aime l'intimité et la proximité physique. Les câlins, les bisous, et plus si affinités... 😏`,
-      category: 'Physique',
+      title: 'Intimité physique et mentale',
+      description: `Il me faut un minimum de complicité ou d'échanges avant de passer à une intimité physique.
+      Un léger côté demi-sexuel ou sapio qui me dit qu'un physique ne fait pas tout (et pas du tout fan de plans cul).`,
     },
     {
       icon: '💭',
-      title: 'Fantaisies',
-      description: `J'ai quelques fantasmes que je partagerai volontiers avec quelqu'un de confiance.`,
-      category: 'Mental',
-    },
-    {
-      icon: '💬',
-      title: 'Communication intime',
-      description: 'Parler de nos désirs, nos limites et nos envies est essentiel pour moi.',
-      category: 'Communication',
+      title: 'Fantasmes',
+      description: `Plutôt vanilla / simple dans l'ensemble, mais pas contre certains fantasmes explorations... expérimentations 🧪​ ?`,
     },
     {
       icon: '⏰',
       title: 'Fréquence',
-      description: `J'aime la régularité dans l'intimité, mais je sais aussi être spontané.`,
-      category: 'Rythme',
+      description: `Ca dépendra de nos libidos ! J'aime beaucoup la spontaniété, pas trop prévoir... et parfois juste des câlins suffisent amplement 🤗​`,
     },
   ];
 
-  public boundaries = [
-    'Le respect mutuel est non-négociable',
-    'Pas de pression, tout doit être consenti',
-    'La communication est la clé',
-    'Chacun ses limites et ses envies',
+  public stats = [
+    { key: 'Taille', value: '180 cm' },
+    { key: 'Poids', value: '120 kg' },
+    { key: 'Muscles', value: '+ des jambes que des bras' },
+    { key: 'Gras', value: 'Un peu partout' },
+    { key: 'Pilosité générale', value: 'Poilu, mais pas trop' },
+    { key: 'Tatouage', value: `Juste sur le bras droit (pour l'instant)` },
   ];
+
+  public otherStats = [
+    { key: 'Pilosité pubienne', value: 'Entretenu' },
+    { key: 'Type', value: 'Pénis de sang' },
+    { key: 'Taille', value: `Dans la moyenne (13-14cm ?)` },
+    { key: 'Épaisseur', value: `Un peu + que la moyenne` },
+    { key: 'Circoncis', value: 'Non' },
+    { key: 'Testicules', value: `Gros mais pas pendants` },
+    { key: 'Zone érogène', value: 'À toi de trouver 😋' },
+  ];
+
+  public likes = [
+    `Fellations (en donner + qu'en recevoir)`,
+    `Doigtage (en faire + que m'en faire)`,
+    'Massages (surtout tantriques)',
+    'Cum-covering',
+    'Shibari (à tester !)',
+  ];
+
+  public boundaries = ['Insultes, surnoms dévalorisants, rabaissements...', 'Anulingus', 'Uro, scato, ...', 'Et à discuter sur le reste ?'];
 
   public revealContent() {
     this.isRevealed = true;
   }
 
-  public hideContent() {
-    this.isRevealed = false;
+  public revealExtraContent() {
+    this.isRevealedExtra = true;
+  }
+
+  public revealVeryExtraContent() {
+    this.isRevealedVeryExtra = true;
+  }
+
+  public openPhoto() {
+    this.showPhoto = true;
+  }
+
+  public closePhoto() {
+    this.showPhoto = false;
   }
 }
