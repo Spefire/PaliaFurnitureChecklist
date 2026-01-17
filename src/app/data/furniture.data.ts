9764f0e5f30f01b6ef5b27c422a364835b8f4f21
Maelle B <marylloup.mb@gmail.com>
2026-01-17 17:06:54 +0100

    Add Investigator Set

--- src/app/data/furniture.data.ts
+++ src/app/data/furniture.data.ts
@@ -2,27 +2,43 @@ import { ICollection } from '@src/models/collection.model';
 
 export const iListCollections: ICollection[] = [
   {
-    name: 'Spooky Moon Store (2023)',
+    name: 'Investigator',
     items: [
       {
-        image: 'https://palia.wiki.gg/images/Spooky_Pumpkin.png?669847',
-        name: 'SpoOOOOOOOOoky Pumpkin',
+        image: 'https://palia.wiki.gg/images/Investigator_Armchair.png?62a082',
+        name: 'Investigator Armchair',
       },
       {
-        image: 'https://palia.wiki.gg/images/Spooky_Pumpkin.png?669847',
-        name: 'Spooky Pumpkin',
+        image: 'https://palia.wiki.gg/images/Investigator_Banner.png?e3bf49',
+        name: 'Investigator Banner',
       },
       {
-        image: 'https://palia.wiki.gg/images/Spooky_Pumpkin.png?669847',
-        name: 'Spooky Pumpkin',
+        image: 'https://palia.wiki.gg/images/Investigator_Bed.png?bb11c0',
+        name: 'Investigator Bed',
       },
       {
-        image: 'https://palia.wiki.gg/images/Spooky_Pumpkin.png?669847',
-        name: 'Spooky Pumpkin',
+        image: 'https://palia.wiki.gg/images/Investigator_Desk.png?d3c38f',
+        name: 'Investigator Desk',
       },
       {
-        image: 'https://palia.wiki.gg/images/Spooky_Pumpkin.png?669847',
-        name: 'Spooky Pumpkin',
+        image: 'https://palia.wiki.gg/images/Investigator_Doormat.png?19e12f',
+        name: 'Investigator Doormat',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Investigator_End_Table.png?fa8da1',
+        name: 'Investigator End Table',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Investigator_Rug.png?bae83b',
+        name: 'Investigator Rug',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Investigator_Runner.png?ce0e4a',
+        name: 'Investigator Runner',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Investigator_Sideboard.png?7d5a2c',
+        name: 'Investigator Sideboard',
       },
     ],
   },
