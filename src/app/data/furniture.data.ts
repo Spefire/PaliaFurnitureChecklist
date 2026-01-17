bf9f0553a265caff6e99110d9c81d3c9efc719d3
Maelle B <marylloup.mb@gmail.com>
2026-01-17 17:26:47 +0100

    add Ravenwood Set

--- src/app/data/furniture.data.ts
+++ src/app/data/furniture.data.ts
@@ -2,7 +2,7 @@ import { ICollection } from '@src/models/collection.model';
 
 export const iListCollections: ICollection[] = [
   {
-    name: 'Investigator',
+    name: 'Investigator Set',
     items: [
       {
         image: 'https://palia.wiki.gg/images/Investigator_Armchair.png?62a082',
@@ -43,23 +43,155 @@ export const iListCollections: ICollection[] = [
     ],
   },
   {
-    name: 'Spooky Moon Store (2023)',
+    name: 'Ravenwood Set',
     items: [
       {
-        image: 'https://palia.wiki.gg/images/Spooky_Pumpkin.png?669847',
-        name: 'Spooky Pumpilyhdflkgbljwdgkin',
+        image: 'https://palia.wiki.gg/images/Ravenwood_Armchair.png?f99ea1',
+        name: 'Ravenwood Armchair',
       },
       {
-        image: 'https://palia.wiki.gg/images/Spooky_Pumpkin.png?669847',
-        name: 'Spooky Pumpkin',
+        image: 'https://palia.wiki.gg/images/Ravenwood_Bathtub.png?d5e33e',
+        name: 'Ravenwood Bathtub',
       },
       {
-        image: 'https://palia.wiki.gg/images/Spooky_Pumpkin.png?669847',
-        name: 'Spooky Pumpkin',
+        image: 'https://palia.wiki.gg/images/Ravenwood_Candelabra.png?1d57b3',
+        name: 'Ravenwood Candelabra',
       },
       {
-        image: 'https://palia.wiki.gg/images/Spooky_Pumpkin.png?669847',
-        name: 'Spooky Pumpkin',
+        image: 'https://palia.wiki.gg/images/Ravenwood_Chandelier.png?a43482',
+        name: 'Ravenwood Chandelier',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Console_Table.png?b712d8',
+        name: 'Ravenwood Console Table',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Couch.png?f3c6a5',
+        name: 'Ravenwood Couch',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Dining_Chair.png?718e11',
+        name: 'Ravenwood Dining Chair',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Dining_Table.png?d4473d',
+        name: 'Ravenwood Dining Table',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Doormat.png?af0410',
+        name: 'Ravenwood Doormat',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Floor_Cabinet.png?ce5100',
+        name: 'Ravenwood Floor Cabinet',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Floor_Decor.png?aa62b4',
+        name: 'Ravenwood Floor Decor',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Flower_Planter.png?d2bc07',
+        name: 'Ravenwood Flower Planter',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Folding_Screen.png?dbcac3',
+        name: 'Ravenwood Folding Screen',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Lantern.png?ebbd2c',
+        name: 'Ravenwood Lantern',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Large_End_Table.png?912e5e',
+        name: 'Ravenwood Large End Table',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Large_Frame_Default_Ingame.png?c34655',
+        name: 'Ravenwood Large Frame',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Medium_Frame_Default_Ingame.png?cb8faf',
+        name: 'Ravenwood Medium Frame',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Mirror.png?9d3fc8',
+        name: 'Ravenwood Mirror',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Nightstand.png?368bf7',
+        name: 'Ravenwood Nightstand',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Pantry.png?e7b4fc',
+        name: 'Ravenwood Pantry',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Runner.png?5d9bb8',
+        name: 'Ravenwood Runner',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Silk_Bed.png?6f9ff2',
+        name: 'Ravenwood Silk Bed',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Small_End_Table.png?88ba76',
+        name: 'Ravenwood Small End Table',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Small_Frame_Default_Ingame.png?257e83',
+        name: 'Ravenwood Small Frame',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Spotlight_Lamp.png?2a6bf3',
+        name: 'Ravenwood Spotlight Lamp',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Standing_Lamp.png?d9bd07',
+        name: 'Ravenwood Standing Lamp',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Toilet.png?762c9d',
+        name: 'Ravenwood Toilet',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Vase.png?e7edc6',
+        name: 'Ravenwood Vase',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Wall_Cabinet.png?98b8a0',
+        name: 'Ravenwood Wall Cabinet',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Wall_Clock.png?a0a3f6',
+        name: 'Ravenwood Wall Clock',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Wall_Lamp.png?52d060',
+        name: 'Ravenwood Wall Lamp',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Wall_Mirror.png?9f17da',
+        name: 'Ravenwood Wall Mirror',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Wall_Shelf.png?b88067',
+        name: 'Ravenwood Wall Shelf',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Wardrobe.png?116b86',
+        name: 'Ravenwood Wardrobe',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Fence_20x.png?dccb3a',
+        name: 'Ravenwood Fence 20x',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Gate.png?9e63b6',
+        name: 'Ravenwood Gate',
+      },
+      {
+        image: 'https://palia.wiki.gg/images/Ravenwood_Column.png?bd1d2e',
+        name: 'Ravenwood Column',
       },
     ],
   },
